f0d0d7179d0b3d9ca4195e34c46fad6520d9b475
jinzhaochao <904448645@qq.com>
2018-04-27 10:41:22 +0800

    【Card】 参数配置优化-添加了stateList的配置

--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -30,89 +30,80 @@
     </div>
 </template>
 <script>
-    const prefixCls = 'lsView-card';
-    const defaultPadding = 16;
-    const stateList = ['待处理', '处理中', '已完结'];
-    export default {
-        name: 'Card',
-        props: {
-            bordered: {
-                type: Boolean,
-                default: true
-            },
-            disHover: {
-                type: Boolean,
-                default: false
-            },
-            shadow: {
-                type: Boolean,
-                default: false
-            },
-            padding: {
-                type: Number,
-                default: defaultPadding
-            },
-            title: {
-                type: String
-            },
-            icon: {
-                type: String
-            },
-            state: {
-                type: Number
-            }
+import { oneOf } from '../../utils/assist';
+const prefixCls = 'lsView-card';
+const defaultPadding = 16;
+export default {
+    name: 'Card',
+    props: {
+        padding: {
+            type: Number,
+            default: defaultPadding
         },
-        data () {
-            return {
-                showHead: true,
-                showFoot: true
-            };
+        title: {
+            type: String
         },
-        computed: {
-            classes () {
-                return [
-                    `${prefixCls}`,
-                    {
-                        [`${prefixCls}-bordered`]: this.bordered && !this.shadow,
-                        [`${prefixCls}-dis-hover`]: this.disHover || this.shadow,
-                        [`${prefixCls}-shadow`]: this.shadow
-                    }
-                ];
-            },
-            headClasses () {
-                return `${prefixCls}-head`;
-            },
-            iconClasses () {
-                return `${prefixCls}-icon`;
-            },
-            bodyClasses () {
-                return `${prefixCls}-body`;
-            },
-            footClasses () {
-                return `${prefixCls}-foot`;
-            },
-            footIconClasses () {
-                return [
-                    `${prefixCls}-foot-icon`,
-                    `${prefixCls}-foot-icon-${this.state}`
-                ];
-            },
-            bodyStyles () {
-                if (this.padding !== defaultPadding) {
-                    return {
-                        padding: `${this.padding}px`
-                    };
-                } else {
-                    return '';
-                }
-            },
-            stage () {
-                return stateList[this.state];
-            }
+        icon: {
+            type: String
         },
-        mounted () {
-            this.showHead = this.title || this.$slots.title !== undefined;
-            this.showFoot = this.state !== undefined || this.$slots.foot !== undefined;
+        state: {
+            validator (value) {
+                return oneOf(value, [0, 1, 2]);
+            },
+            type: Number,
+            default:0
+        },
+        stateList:{
+            type:Array,
+            default:['待处理', '处理中', '已完结']
         }
-    };
+    },
+    data () {
+        return {
+            showHead: true,
+            showFoot: true
+        };
+    },
+    computed: {
+        classes () {
+            return [
+                `${prefixCls}`
+            ];
+        },
+        headClasses () {
+            return `${prefixCls}-head`;
+        },
+        iconClasses () {
+            return `${prefixCls}-icon`;
+        },
+        bodyClasses () {
+            return `${prefixCls}-body`;
+        },
+        footClasses () {
+            return `${prefixCls}-foot`;
+        },
+        footIconClasses () {
+            return [
+                `${prefixCls}-foot-icon`,
+                `${prefixCls}-foot-icon-${this.state}`
+            ];
+        },
+        bodyStyles () {
+            if (this.padding !== defaultPadding) {
+                return {
+                    padding: `${this.padding}px`
+                };
+            } else {
+                return '';
+            }
+        },
+        stage () {
+            return this.stateList[this.state];
+        }
+    },
+    mounted () {
+        this.showHead = this.title || this.$slots.title !== undefined;
+        this.showFoot = this.state !== undefined || this.$slots.foot !== undefined;
+    }
+};
 </script>
